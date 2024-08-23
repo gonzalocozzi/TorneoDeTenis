d95d126bd47102b2b8a63614a1d6fbf2ea3524fa
Gonzalo Alejandro Cozzi <gcozzi@outlook.com.ar>
2024-08-23 11:59:41 -0300

    Se registran el contexto de base de datos y los repositorios como servicios en el proyecto de webapi

--- TorneoDeTenis.WebApi/Program.cs
+++ TorneoDeTenis.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using TorneoDeTenis.WebApi.Infraestructure.Data;
 using TorneoDeTenis.WebApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +15,11 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IEnfrentamientoStrategyFactory, EnfrentamientoStrategyFactory>();
 builder.Services.AddSingleton<IRandomProvider, RandomProvider>();
 builder.Services.AddScoped<ITorneoService, TorneoService>();
+
+// Connect to the Database context and repositories
+builder.Services.AddDbContext<TorneoDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<ITorneoRepository, TorneoRepository>();
+builder.Services.AddScoped<IJugadorRepository, JugadorRepository>();
 
 var app = builder.Build();
 
